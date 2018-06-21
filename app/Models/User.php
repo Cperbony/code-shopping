2f189fbbd2f3caf0dec67e35c2781829ae95ac49
Claudinei Perboni <cperbony@gmail.com>
2018-06-21 16:04:36 -0300

    Iniciando autenticação JWT

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,8 +5,9 @@ namespace CodeShopping\Models;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable, SoftDeletes;
 
@@ -39,5 +40,28 @@ class User extends Authenticatable
     {
         !isset($attributes['password']) ?: $attributes['password'] = bcrypt($attributes['password']);
         return parent::fill($attributes);
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [
+            'email' => $this->email,
+            'name' => $this->name
+        ];
     }
 }
