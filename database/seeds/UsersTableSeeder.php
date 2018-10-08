4c3e754a8b18dce266c6de951a9a669b2c6f6348
Claudinei Perboni <cperbony@gmail.com>
2018-10-08 15:17:52 -0300

    Melhoria na seeder de usuários

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use CodeShopping\Models\User;
+use CodeShopping\Models\UserProfile;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -12,22 +14,55 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        \File::deleteDirectory(UserProfile::photosPath(), true);
         factory(User::class, 1)
             ->create([
                 'email' => 'admin@user.com'
             ])
             ->each(function ($user) {
-                $user->profile->phone_number = '+16505551234';
-                $user->profile->save();
+                Model::reguard();
+                $user->updateWithProfile([
+                    'phone_number' => '+16505551234',
+                    'photo' => $this->getAdminPhoto()
+                ]);
+                Model::unguard();
             });
+
+
         factory(User::class, 1)
             ->create([
                 'email' => 'customer@user.com',
                 'role' => User::ROLE_CUSTOMER
-            ]);
+            ])
+            ->each(function ($user) {
+                Model::reguard();
+                $user->updateWithProfile([
+                    'phone_number' => '+16505551235',
+                    'photo' => $this->getCustomersPhoto()
+                ]);
+                Model::unguard();
+            });
+
+
         factory(User::class, 50)
             ->create([
                 'role' => User::ROLE_CUSTOMER
             ]);
+    }
+
+    public function getAdminPhoto()
+    {
+        return new \Illuminate\Http\UploadedFile(
+            storage_path('app/faker/users_admin/trump.jpg'),
+            str_random(16) . '.jpg'
+        );
+    }
+
+    public function getCustomersPhoto()
+    {
+        return new \Illuminate\Http\UploadedFile(
+            storage_path('app/faker/users_customers/bruce.jpg'),
+            str_random(16) . '.jpg'
+        );
     }
 }
